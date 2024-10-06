99e2ac3a3e2c48ecb552f7dbeb1a6253c90e9d15
wille-wang <130608029+wille-wang@users.noreply.github.com>
2024-10-06 19:48:06 +1100

    perf: set the initial location to UniMelb

--- app/src/main/java/com/example/myapplication/ui/map/MapFragment.java
+++ app/src/main/java/com/example/myapplication/ui/map/MapFragment.java
@@ -7,7 +7,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import com.example.myapplication.R; // Ensure this import is correct
+import com.example.myapplication.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,9 +21,17 @@ public class MapFragment extends Fragment {
       new OnMapReadyCallback() {
         @Override
         public void onMapReady(GoogleMap googleMap) {
-          LatLng sydney = new LatLng(-34, 151);
-          googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-          googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+          // Set the coordinates for University of Melbourne, Parkville Campus
+          LatLng melbourneUni = new LatLng(-37.7963, 144.9614);
+
+          // Add a marker for University of Melbourne
+          googleMap.addMarker(
+              new MarkerOptions()
+                  .position(melbourneUni)
+                  .title("University of Melbourne, Parkville Campus"));
+
+          // Move and zoom the camera to the University of Melbourne
+          googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(melbourneUni, 15));
         }
       };
 
