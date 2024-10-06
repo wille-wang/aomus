9611286f90eeeca14fe33d74fc6b7e16dc603951
wille-wang <130608029+wille-wang@users.noreply.github.com>
2024-10-06 22:39:54 +1100

    feat: see suggestions about defined locations

--- app/src/main/java/com/example/myapplication/ui/map/MapFragment.java
+++ app/src/main/java/com/example/myapplication/ui/map/MapFragment.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SearchView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,11 +16,23 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapFragment extends Fragment {
 
+  private final Map<String, LatLng> locationMap =
+      new HashMap<String, LatLng>() {
+        {
+          // mock data for locations
+          put("University of Melbourne, Parkville Campus", new LatLng(-37.7963, 144.9614));
+          put("Melbourne Central", new LatLng(-37.8100, 144.9628));
+          put("Flinders Street Station", new LatLng(-37.8183, 144.9671));
+          put("Federation Square", new LatLng(-37.8179, 144.9691));
+        }
+      };
   private GoogleMap mMap;
-
   private final OnMapReadyCallback callback =
       new OnMapReadyCallback() {
         @Override
@@ -32,18 +45,21 @@ public class MapFragment extends Fragment {
           mMap.getUiSettings().setMapToolbarEnabled(true);
 
           // Set the coordinates for University of Melbourne, Parkville Campus
-          LatLng melbourneUni = new LatLng(-37.7963, 144.9614);
+          LatLng melbourneUni = locationMap.get("University of Melbourne, Parkville Campus");
 
           // Add a marker for University of Melbourne
-          mMap.addMarker(
-              new MarkerOptions()
-                  .position(melbourneUni)
-                  .title("University of Melbourne, Parkville Campus"));
+          if (melbourneUni != null) {
+            mMap.addMarker(
+                new MarkerOptions()
+                    .position(melbourneUni)
+                    .title("University of Melbourne, Parkville Campus"));
 
-          // Move and zoom the camera to the University of Melbourne
-          mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(melbourneUni, 15));
+            // Move and zoom the camera to the University of Melbourne
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(melbourneUni, 15));
+          }
         }
       };
+  private ArrayAdapter<String> suggestionAdapter;
 
   @Nullable
   @Override
@@ -53,27 +69,39 @@ public class MapFragment extends Fragment {
       @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-    // Initialize the search bar
-    SearchView searchView = view.findViewById(R.id.searchView);
+    // Initialize the AutoCompleteTextView
+    AutoCompleteTextView autoCompleteTextView = view.findViewById(R.id.autoCompleteTextView);
 
-    // Expand the search view by default
-    searchView.setIconified(false);
-    searchView.clearFocus();
+    // Ensure the AutoCompleteTextView is not null
+    if (autoCompleteTextView != null) {
+      // Make sure AutoCompleteTextView is focusable and can receive input
+      autoCompleteTextView.setFocusable(true);
+      autoCompleteTextView.setFocusableInTouchMode(true);
 
-    searchView.setOnQueryTextListener(
-        new SearchView.OnQueryTextListener() {
-          @Override
-          public boolean onQueryTextSubmit(String query) {
-            // Handle search query submission
-            // For example, you can use Geocoder to find the location and move the map
-            return false;
-          }
+      // Create an ArrayAdapter to hold suggestions
+      suggestionAdapter =
+          new ArrayAdapter<>(
+              getContext(),
+              android.R.layout.simple_dropdown_item_1line,
+              new ArrayList<>(locationMap.keySet()));
 
-          @Override
-          public boolean onQueryTextChange(String newText) {
-            return false;
-          }
-        });
+      // Set the adapter to the AutoCompleteTextView
+      autoCompleteTextView.setAdapter(suggestionAdapter);
+
+      // Ensure suggestions are shown after typing 1 character
+      autoCompleteTextView.setThreshold(1);
+
+      // Handle item selection from suggestions
+      autoCompleteTextView.setOnItemClickListener(
+          (parent, view1, position, id) -> {
+            String selectedLocation = (String) parent.getItemAtPosition(position);
+            LatLng location = locationMap.get(selectedLocation);
+            if (location != null) {
+              mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+              mMap.addMarker(new MarkerOptions().position(location).title(selectedLocation));
+            }
+          });
+    }
 
     return view;
   }
