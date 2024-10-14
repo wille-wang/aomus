879d93c9967df9f898e8c2b4b86ed5c443201222
zeyuexu <zeyue@student.unimelb.edu.au>
2024-10-14 14:54:54 +1100

    feat: add route feature page

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -26,6 +26,7 @@ import com.example.myapplication.ui.library.LibraryFragment;
 import com.example.myapplication.ui.login.LoginActivity;
 import com.example.myapplication.ui.map.MapFragment;
 import com.example.myapplication.ui.scanner.ScannerFragment;
+import com.example.myapplication.ui.route.RouteFragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity
@@ -62,7 +63,8 @@ public class MainActivity extends AppCompatActivity
                 R.id.nav_library,
                 R.id.nav_scanner,
                 R.id.nav_map,
-                R.id.nav_chatbot)
+                R.id.nav_chatbot,
+                R.id.nav_route)
             .setOpenableLayout(drawer)
             .build();
     NavController navController =
@@ -146,6 +148,11 @@ public class MainActivity extends AppCompatActivity
           .beginTransaction()
           .replace(R.id.nav_host_fragment_content_main, new ChatbotFragment())
           .commit();
+    } else if (id == R.id.nav_route) {
+      getSupportFragmentManager()
+              .beginTransaction()
+              .replace(R.id.nav_host_fragment_content_main, new RouteFragment())
+              .commit();
     }
 
     DrawerLayout drawer = findViewById(R.id.drawer_layout);
---
+++ app/src/main/java/com/example/myapplication/ui/route/RouteFragment.java
@@ -0,0 +1,6 @@
+package com.example.myapplication.ui.route;
+
+import androidx.fragment.app.Fragment;
+
+public class RouteFragment extends Fragment {
+}
